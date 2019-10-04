f44d9c7bcdd074fe18f15b301d06d8157ffe64ee
user <zliden01@protonmail.com>
2019-10-04 21:06:44 +0300

    feat: resualt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    // your solution
+    let a = first;
+    let b = second;
+
+    return (BigInt(a) * BigInt(b)).toString();
 }
